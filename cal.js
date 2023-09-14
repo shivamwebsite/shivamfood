f734e2ac518fcbc10a95d482dbe5caff9f6b7064
shivamwebsite <141153430+shivamwebsite@users.noreply.github.com>
2023-09-14 08:48:04 +0530

    DONE

--- cal.js
+++ cal.js
@@ -4,22 +4,22 @@ let string="";
 let arr=Array.from(buttons);
 arr.forEach((button) =>{
     button.addEventListener('click',(e) =>{
-        // if(e.target.innerHTML == '='){
-        //     string = eval(string);
-        //     input.value=string;
-        // }
-        // // else if(e.target.innerHTML == 'AC'){
-        // //     string="";
-        // //     input.value=string;
-        // // }
-        // // else if(e.target.innerHTML == 'clr'){
-        // //     string=string.substring(0,string.length-1);
-        // //     input.value=string;
-        // // }
-        // else{
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            input.value=string;
+        }
+        else if(e.target.innerHTML == 'AC'){
+            string="";
+            input.value=string;
+        }
+        else if(e.target.innerHTML == 'clr'){
+            string=string.substring(0,string.length-1);
+            input.value=string;
+        }
+        else{
             console.log(e.target);
             string +=e.target.innerHTML;
             input.value=string;
-        // }
+        }
     })
 })
